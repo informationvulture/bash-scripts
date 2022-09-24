1cde2eb8e049491cb8e48b06c440213c3339e357
informationvulture <schoolemailnm1@gmail.com>
2022-09-24 00:31:37 +0000

    Add more comments

--- tat.sh
+++ tat.sh
@@ -3,18 +3,23 @@
 # Print the total failed login attempts.
 
 
+# Store the log file in variable, where we can use it later.
 readonly LOG_FILE=$(sudo cat /var/log/auth.log)
 
 
+# The number of attempted logins, based on the number of lines.
 attempts=$(printf "$LOG_FILE" | grep Failed | wc -l)
 
 
+# The start time of the log file.
 beg_time=$(printf "$LOG_FILE" | head -1 | cut -f1-3 -d' ')
 
 
+# The end time of the log file.
 end_time=$(printf "$LOG_FILE" | tail -1 | cut -f1-3 -d' ')
 
 
+# Print the useful info.
 printf "From $beg_time to $end_time: \n"
 
 
